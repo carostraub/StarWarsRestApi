f76a9a5301a85533dfd7f042fce8c4e6ac194ddc
carostraub <carolina.straub@gmail.com>
2025-02-20 00:23:09 +0000

    StarWars API

--- src/app.py
+++ src/app.py
@@ -9,6 +9,9 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
+from models import db, Personaje
+from models import db, Planeta
+from models import db, Favorito
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +39,155 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    resultado = [user.serialize() for user in users]
+    return jsonify(resultado), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/personajes', methods=['GET'])
+def obtener_personajes():
 
-    return jsonify(response_body), 200
+    personajes = Personaje.query.all() 
+    personajes_serializable = list(map(lambda pe: pe.to_dict(), personajes))
+    return jsonify(personajes_serializable), 200
+
+def agregar_personaje():
+
+    datos = request.get_json() 
+
+    if not 'name' in datos or not datos['name']:
+        return jsonify({ "msg": "Field name is required"}), 400
+    if not 'specie' in datos or not datos['specie']:
+        return jsonify({ "msg": "Field specie is required"}), 400
+    if not 'height' in datos or not datos['height']:
+        return jsonify({ "msg": "Field height is required"}), 400
+    if not 'birth_year' in datos or not datos['birth_year']:
+        return jsonify({ "msg": "Field birth year is required"}), 400
+    if not 'gender' in datos or not datos['gender']:
+        return jsonify({ "msg": "Field gender is required"}), 400
+
+    personaje = Personaje()
+    personaje.name = datos['name']
+    personaje.specie = datos['specie']
+    personaje.height = datos['height']
+    personaje.birth_year = datos['birth_year']
+    personaje.gender = datos['gender']
+    personaje.save()
+
+   
+
+    return jsonify({ "status": "success", "message": "Personaje Agregado"}), 201
+
+
+@app.route('/personajes/<int:personaje_id>', methods=['GET'])
+def persona_especifico(persona_id):
+    persona = Personaje.query.get(persona_id)
+    personaje_serializable = list(map(lambda persojaesp: persojaesp.to_dict(), persona))
+    return jsonify(personaje_serializable), 200
+
+@app.route('/planetas', methods=['GET'])
+def obtener_planetas():
+
+    planetas = Planeta.query.all() 
+    planetas_serializable = list(map(lambda pl: pl.to_dict(), planetas))
+    return jsonify(planetas_serializable), 200
+
+def agregar_planetas():
+
+    datos = request.get_json() 
+
+    if not 'name' in datos or not datos['name']:
+        return jsonify({ "msg": "Field name is required"}), 400
+    if not 'climate' in datos or not datos['climate']:
+        return jsonify({ "msg": "Field climate is required"}), 400
+    if not 'population' in datos or not datos['population']:
+        return jsonify({ "msg": "Field population is required"}), 400
+    if not 'terreno' in datos or not datos['terreno']:
+        return jsonify({ "msg": "Field terreno is required"}), 400
+
+    planeta = Planeta()
+    planeta.name = datos['name']
+    planeta.climate = datos['climate']
+    planeta.population = datos['population']
+    planeta.terreno = datos['terreno']
+    planeta.save()
+
+   
+
+    return jsonify({ "status": "success", "message": "Planeta Agregado"}), 201
+
+@app.route('/planetas/<int:planeta_id>', methods=['GET'])
+def planeta_especifico(planeta_id):
+    planet = Planeta.query.get(planeta_id)
+    planeta_serializable = list(map(lambda planetesp: planetesp.to_dict(), planet))
+    return jsonify(planeta_serializable), 200
+
+
+@app.route('/users/favoritos', methods=['GET'])
+def obtener_favoritos():
+
+    favoritos = Favorito.query.all() 
+    favoritos_serializable = list(map(lambda f: f.to_dict(), favoritos))
+    return jsonify(favoritos_serializable), 200
+
+@app.route('/favoritos/planetas/<int:planeta_id>', methods=['POST'])
+def agregar_favoritospl():
+
+    username = request.json.get("username") 
+    user = User.query.get(username)
+    planeta = Planeta.query.get(id)
+
+    favorito = Favorito(username=user.username, planeta_id=id)
+    db.session.add(favorito)
+    db.session.commit()
+
+    return jsonify({"msg":f"Planeta: {planeta.name} se agrego a tus favoritos"})
+
+
+@app.route('/favoritos/personajes/<int:personaje_id>', methods=['POST'])
+def agregar_favoritospe():
+
+    username = request.json.get("username") 
+    user = User.query.get(username)
+    personaje = Personaje.query.get(id)
+
+    favorito = Favorito(username=user.username, personaje_id=id)
+    db.session.add(favorito)
+    db.session.commit()
+    
+    return jsonify({"msg":f"Personaje: {personaje.name} se agrego a tus favoritos"})
+   
+
+@app.route('/favoritos/planetas/<int:planeta_id>', methods=['DELETE'])
+def eliminar_favoritospl():
+
+    username = request.json.get("username") 
+    user = User.query.get(username)
+    planeta = Planeta.query.get(id)
+
+    favorito = Favorito.query.filter_by(username=user.username, planeta_id=id)
+    db.session.delete(favorito)
+    db.session.commit()
+
+    return jsonify({"msg":f"Planeta: {planeta.name} se elimino a tus favoritos"})
+
+
+@app.route('/favoritos/personajes/<int:personaje_id>', methods=['DELETE'])
+def eliminar_favoritospe():
+
+    username = request.json.get("username") 
+    user = User.query.get(username)
+    personaje = Personaje.query.get(id)
+
+    favorito = Favorito.query.filter_by(username=user.username, personaje_id=id)
+    db.session.delete(favorito)
+    db.session.commit()
+
+    return jsonify({"msg":f"Personaje: {personaje.name} se elimino a tus favoritos"})
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,107 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    active = db.Column(db.Boolean())
+
+    favs = db.relationship("Favorito", backref='user')
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            
         }
+    
+class Personaje(db.Model):
+    __tablename__ ='personajes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    specie = db.Column(db.String(200), nullable=False)
+    height = db.Column(db.String, nullable=False)
+    birth_year = db.Column(db.String, nullable=False)
+    gender = db.Column(db.String, nullable=False)
+
+    favorite = db.relationship("Favorito", backref='personajes')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "specie": self.specie,
+            "height": self.height,
+            "birth_year": self.birth_year,
+            "gender": self.gender
+        }
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Planeta(db.Model):
+    __tablename__ ='planetas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    climate = db.Column(db.String(200), nullable=False)
+    population = db.Column(db.String, nullable=False)
+    terreno = db.Column(db.String, nullable=False)
+
+    favoritos = db.relationship("Favorito", backref='planetas')
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "terreno": self.terreno
+        }
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Favorito(db.Model):
+    __tablename__ ='favoritos'
+    id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    favs_personajes = db.Column(db.Integer, db.ForeignKey('personajes.id'), primary_key=True)
+    favs_planetas = db.Column(db.Integer, db.ForeignKey('planetas.id'), primary_key=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "favs_personajes": self.favs_personajes,
+            "favs_planetas": self.favs_planetas,
+            
+        }
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    
+
+
